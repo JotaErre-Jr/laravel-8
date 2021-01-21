e11e5066bf15c2fb168884cdd5f6998a7398e606
Junior Jotaere <rochajotaerre@gmail.com>
2021-01-21 13:01:53 -0300

    corrigido as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,4 @@ use Illuminate\Support\Facades\Route;
     #return view('welcome');
 });*/
 
-Route::get('/', ['uses' => 'Controller@homepage']);
+Route::get('/', [Controller::class, 'homepage']);
